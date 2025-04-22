837b18cc6d38f5c64a9d5e6e3c7c62a2a8049308
KM <sammy.koo90@gmail.com>
2025-04-22 14:11:12 +0900

    테스트

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,4 +1,5 @@
 import TempPage from "@/app/PageTemplates/page";
+import Form from "next/form";
 
 export const metadata = {
   title: '대시보드',
@@ -8,6 +9,10 @@ export default function DashboardPage() {
   return (
     <div className="min-h-screen">
       <TempPage />
+      <Form action='/api/test' >
+        <input type="text" name="test" className="p-2 my-2 rounded-lg bg-neutral-600" placeholder="테스트" />
+        <button type="submit" className="border rounded">GO</button>
+      </Form>
     </div>
   );
 }
