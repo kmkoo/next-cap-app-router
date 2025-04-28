7fe1ada56329940db8f3fde68efb64393276be82
kmkoo <sammy.koo90@gmail.com>
2025-04-28 22:52:15 +0900

    로그인 페이지 레이아웃 수정 및 디자인 개선

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,11 +4,13 @@ export const metadata = {
 
 export default function authPage() {
   return (
-    <div className="min-h-screen">
-      <div className="border bg-green-300 rounded-r-full h-150 w-250 z-1 mt-30"/>
-      <div className="justify-items-center items-center">
-        <p className="font-bold text-4xl">원하는 서버를 바로 생성하고,<br/>더 나은 환경을 경험하세요</p>
+    <div className="min-h-screen static">
+      <div className="bg-green-300 rounded-r-full h-3/5 w-2/4 fixed top-1/10">
+        <div className="justify-items-center m-30">
+          <p className="font-bold text-4xl">원하는 서버를 바로 생성하고<br/>더 나은 환경을 경험하세요<br/><br/>아 디자인 미치겠네 진짜;;</p>
+        </div>
       </div>
+      <div className="bg-yellow-300/80 rounded-l-full h-1/3 w-1/2 z-1 fixed right-70 top-1/2"/>
     </div>
   );
 }
