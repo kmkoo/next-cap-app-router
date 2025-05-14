a57e875d1b9c1f91650628a0e516b1568b6f2bb2
kmkoo <sammy.koo90@gmail.com>
2025-05-14 19:09:14 +0900

    aws-ec2 함수 생성

--- src/lib/aws-ec2.ts
+++ src/lib/aws-ec2.ts
@@ -1,10 +1,11 @@
-import { DescribeInstancesCommand, EC2Client, RunInstancesCommand } from "@aws-sdk/client-ec2";
+import { _InstanceType, DescribeInstancesCommand, EC2Client, RunInstancesCommand, StartInstancesCommand, StopInstancesCommand, TerminateInstancesCommand } from "@aws-sdk/client-ec2";
 
 const ec2Client = new EC2Client({ region: process.env.AWS_REGION });
 
-export async function getInstanceData(instanceId:string) {
+export async function getInstanceData(instanceIds:string[]) {
   const command = new DescribeInstancesCommand({
-    InstanceIds: [instanceId],
+    InstanceIds: instanceIds,
+    DryRun: true,  // 실제 작동 방지
   });
 
   const response = await ec2Client.send(command);
@@ -15,11 +16,12 @@ export async function getInstanceData(instanceId:string) {
 
 export async function createInstance(params:{
   imageId: string,
-  keyName?: string,
-  tagName?: string,
+  instanceType: _InstanceType,
+  serverTag: string,
+  serverOwner: string,
   userCommand?: string,
 }) {
-  const { imageId, keyName, tagName, userCommand } = params;
+  const { imageId, instanceType, serverTag, serverOwner, userCommand } = params;
 
   // 서버가 생성된 후 실행할 명령어를 command에 받아옴
 	const script = `#!/bin/bash\n${userCommand}`;
@@ -27,21 +29,53 @@ export async function createInstance(params:{
 
   const command = new RunInstancesCommand({
     ImageId: imageId,
-    InstanceType: "t3.small",
+    InstanceType: instanceType,
     MinCount: 1,
     MaxCount: 1,
-    KeyName: keyName,
-    TagSpecifications: tagName ? [
-    {
+    TagSpecifications: [{
       ResourceType: 'instance',
       Tags: [
-        { Key: "name", Value: tagName }
+        { Key: "Name", Value: "squirrel-ec2" },
+        { Key: "group", Value: "capstone" },
+        { Key: "username", Value: "capstone-squirrel" },
+        { Key: "serviceName", Value: serverTag },
+        { Key: "owner", Value: serverOwner },
       ]
-    }
-    ] : undefined,
+    }],
 		UserData: userData,
+    DryRun: true,  // 실제 작동 방지
   });
   
   const response = await ec2Client.send(command);
-  return response.Instances?.[0];
+  return response.Instances;
+}
+
+export async function startInstance(instanceIds:string[]) {
+  const command = new StartInstancesCommand({
+    InstanceIds: instanceIds,
+    DryRun: true,  // 실제 작동 방지
+  });
+
+  const response = await ec2Client.send(command);
+  return response.StartingInstances;
+}
+
+export async function stopInstance(instanceIds:string[]) {
+  const command = new StopInstancesCommand({
+    InstanceIds: instanceIds,
+    DryRun: true,  // 실제 작동 방지
+  });
+  
+  const response = await ec2Client.send(command);
+  return response.StoppingInstances;
+}
+
+export async function terminateInstance(instanceIds:string[]) {
+  const command = new TerminateInstancesCommand({
+    InstanceIds: instanceIds,
+    DryRun: true,  // 실제 작동 방지
+  });
+
+  const response = await ec2Client.send(command);
+  return response.TerminatingInstances;
 }
