56b0bffe5f19f251c0b1a4b992bc4c3e40e960d3
kmkoo <sammy.koo90@gmail.com>
2025-05-27 22:27:05 +0900

    인스턴스 생성시 DB저장 기능 추가

--- src/app/api/aws/ec2/create/route.ts
+++ src/app/api/aws/ec2/create/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createInstance } from '@/lib/aws-ec2';
 import { _InstanceType } from "@aws-sdk/client-ec2";
-import { select_Server } from "@/lib/db";
+import db from "@/lib/dbcon";
+import { RowDataPacket } from "mysql2";
 
 export async function POST(req: NextRequest) {
   let instanceType: _InstanceType;
+  type ExistRow = RowDataPacket & { isExist: number };
   
   try {
     const body = await req.json();
@@ -12,16 +14,27 @@ export async function POST(req: NextRequest) {
 
 
     if (!serverOwner || !serverName || !serverScale) {
-      return NextResponse.json({ success: false, error: "Missing parameters" }, { status: 400 });
+      return NextResponse.json({ success: false, errorMassage: "Missing parameters" }, { status: 400 });
     }
-    
-    // if (serverOwner === select_User("SELECT userName FROM User ")) { // DB 조회 -> 없는 사용자면 거부
-    //   return NextResponse.json({ success: false, error: "알수 없는 사용자 입니다." }, { status: 400 });
-    // }
 
-    if (serverName === select_Server(serverName)) { // DB 조회 -> 없는 사용자면 거부
-      return NextResponse.json({ success: false, error: "중복된 서버 이름 입니다." }, { status: 400 });
+    const [userRow] = await db.query<ExistRow[]>(
+      "SELECT EXISTS (SELECT 1 FROM User WHERE userName = ?) AS isExist",
+      [serverOwner]
+    );
+    const [serverRow] = await db.query<ExistRow[]>(
+      "SELECT EXISTS (SELECT 1 FROM Server WHERE serverName = ?) AS isExist",
+      [serverName]
+    );
+
+    if (userRow[0].isExist === 0) { // DB 조회 -> 없는 사용자면 거부
+      return NextResponse.json({ success: false, errorMassage: "알수 없는 사용자 입니다." }, { status: 400 });
     }
+
+    if (serverRow[0].isExist === 1) { // DB 조회 -> 이미 있는 서버 이름이면 거부
+      return NextResponse.json({ success: false, errorMassage: "중복된 서버 이름 입니다." }, { status: 400 });
+    }
+
+    console.log("서버 생성 가능");
 
     switch(serverScale){
       case 'small':
@@ -34,12 +47,20 @@ export async function POST(req: NextRequest) {
         instanceType = "t3.medium"
         break;
       default:
-        return NextResponse.json({ success: false, error: "잘못된 서버 스케일" }, { status: 400 }); 
+        return NextResponse.json({ success: false, errorMassage: "잘못된 서버 스케일" }, { status: 400 }); 
     }
 
-    const instance = await createInstance({ instanceType, serverTag:serverName, serverOwner });
+    const instance = await createInstance({ instanceType, serverTag:serverName, serverOwner }); // 인스턴스 생성
+    await db.query( // 인스턴스 생성 후 DB에 입력
+      `INSERT INTO Server (userNumber, serverName, serverType)
+      SELECT userNumber, ?, ?
+      FROM User
+      WHERE userName = ?`,
+      [serverName, instanceType, serverOwner]
+    );
     return NextResponse.json({ success: true, instance });
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ success: false, error: (error as Error).message }, { status: 500 });
   }
 	
