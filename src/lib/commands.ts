43f713e8b73fe53cd5bdb22a69ce5bfe9758ff28
kmkoo <sammy.koo90@gmail.com>
2025-06-15 22:11:27 +0900

    7트 오타수정

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -11,7 +11,7 @@ echo "SQUD [info] 서버 실행준비 완료"
 echo "SQUD [info] 게임서버를 서비스로 등록합니다."
 
 cat << EOF > /home/ec2-user/minecraft/auto-starter.sh
-#1/bin/bash
+#!/bin/bash
 cd /home/ec2-user/minecraft
 nohup java -Xmx1024M -Xms1024M -jar minecraft_server.1.21.5.jar nogui &
 EOF
@@ -23,23 +23,22 @@ cat << EOF > /etc/systemd/system/minecraft.service
 Description=Minecraft Server
 After=network.target
 
-[service]
+[Service]
 Type=simple
 User=ec2-user
 WorkingDirectory=/home/ec2-user/minecraft
-ExecStart/home/ec2-user/minecraft/autostarter.sh
+ExecStart=/home/ec2-user/minecraft/autostarter.sh
 Restart=on-failure
 
 [Install]
-WantedBy=nulti-user.target
+WantedBy=multi-user.target
 EOF
     
 systemctl daemon-reexec
 systemctl daemon-reload
 systemctl enable minecraft
 systemctl start minecraft
-echo "SQUD [info] 모든 준비가 완료되어 서버를 실행합니다."
-    `
+echo "SQUD [info] 모든 준비가 완료되어 서버를 실행합니다."`
   ,
 //   추가할거 있으면 이렇게:
 //     "하고 여기에 명령어"
